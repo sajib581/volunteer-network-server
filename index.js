02ddc8a729512ee3802b3d21689697ac90aef46e
sajib121 <sajibdas581@gmail.com>
2020-10-05 23:24:54 +0600

    port and nodemon fixed

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
-const port = 5000
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -80,6 +80,6 @@ client.connect(err => {
   console.log("Database Connected Successfully");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
